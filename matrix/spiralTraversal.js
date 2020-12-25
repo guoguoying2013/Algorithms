d7dd2838c09a602630b4cb67cc14f9831550fe53
Ying Guo <guoguoying2013@gmail.com>
2020-12-24 18:51:11 -0800

    spiralTraversal is done

--- matrix/spiralTraversal.js
+++ matrix/spiralTraversal.js
@@ -14,11 +14,58 @@
     returns [1, 2, 3, 6, 9, 8, 7, 4, 5]
  */
 
-var spiralTraversal = function(matrix) {
 
-    // TODO: Implement me!
-    let m = matrix.length;
-    let n = matrix[0].length;
-  
-    
+ // edge case 1: 
+ //     [[1], [2], [3]]
+ // edge case 2:
+ //     [[1, 2, 3]]
+ // time complexity: O(n), n i the total number of elements in this input matrix. 
+ // space complexity: O(n), information stored in variable results
+var spiralTraversal = function(matrix) {
+    let startRowIndex = 0;
+    let endRowIndex= matrix.length - 1;
+    let startColIndex = 0;
+    let endColIndex = matrix[0].length - 1;
+    let results = [];
+
+    while (startRowIndex <= endRowIndex && startColIndex <= endColIndex) {
+        for (var i = startColIndex; i <= endColIndex; i++) {
+            results.push(matrix[startRowIndex][i]);
+        }
+        startRowIndex++;
+
+        for (var j = startRowIndex; j <= endRowIndex; j++) {
+            results.push(matrix[j][endColIndex]);
+        }
+        endColIndex--;
+
+        if (startRowIndex <= endRowIndex) {
+            for (var k = endColIndex; k >= startColIndex; k--) {
+                results.push(matrix[endRowIndex][k]);
+            }
+            endRowIndex--;
+        }
+
+        if (startColIndex <= endColIndex) {
+            for (var m = endRowIndex; m >= startRowIndex; m--) {
+                results.push(matrix[m][startColIndex]);
+            }
+            startColIndex++;
+        }
+    }
+
+    return results;
   };
+
+  // testing
+  const matrix1 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+  ];
+  const matrix2 = [[1], [2], [3]];
+  const matrix3 = [[1, 2, 3]];
+
+  console.log(spiralTraversal(matrix1));
+  console.log(spiralTraversal(matrix2));
+  console.log(spiralTraversal(matrix3));
