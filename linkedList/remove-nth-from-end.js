2ec50502cc5210aceb7d6d849aa2e626caf0e464
Ying Guo <guoguoying2013@gmail.com>
2021-02-07 23:57:29 -0800

    remove-nth-from-end.js

---
+++ linkedList/remove-nth-from-end.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  var nullHead = new ListNode(null);
+  nullHead.next = head;
+  var p1 = nullHead;
+  var p2 = nullHead;
+    
+  for(var i = 0; i < n + 1; i++)
+    p1 = p1.next;
+  while(p1 !== null){
+    p2 = p2.next;
+    p1 = p1.next;
+  }
+  p2.next = p2.next.next;
+  return nullHead.next;
+};
