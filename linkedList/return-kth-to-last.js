8df25b3e00e1be46552585833ed5ea1ccbf960e1
Ying Guo <guoguoying2013@gmail.com>
2021-02-07 23:59:40 -0800

    added return-keth-to-last.js

---
+++ linkedList/return-kth-to-last.js
@@ -0,0 +1,13 @@
+var removeNthFromEnd = function(head, n) {
+    let p1 = head;
+    let p2 = head;
+    for (let i = 0; i < n; i++) {
+      if (p1 == null) return null;
+      p1 = p1.next;
+    }
+    while (p1 != null) {
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+    return p2;
+};
