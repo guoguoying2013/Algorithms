2807004057e2668efc8a0a6694355c451efd6b55
Ying Guo <guoguoying2013@gmail.com>
2021-02-14 17:34:58 -0800

    linked list

---
+++ linkedList/middle-of-a-linked-list.js
@@ -0,0 +1,24 @@
+// Given a non-empty, singly linked list with head node head,
+// return a middle node of linked list.
+// If there are two middle nodes, return the second middle node.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
